97138df8fa232c6f3a59c1626a9c12d29a0e76fd
Luca-Sberna <sberna.luca.ibiza@gmail.com>
2023-02-06 13:15:12 +0100

    s2-g1-teoria

---
+++ m1/s2/g1/teoria/js/script.js
@@ -0,0 +1,11 @@
+/*commento su più righe
+ */
+
+//commento a riga singola
+
+// ; = idnica la fine di un istruzione
+
+//gli script rallentano la pagina se messi nell head a meno che non abbiamo un indicazione specifica da parte del browser
+
+document.getElementById("titolo").textContent = "Hello World!";
+//dal documento - (funzione) prendimi un elemento id - scrivimi/creami un testo dentro un elemento =
