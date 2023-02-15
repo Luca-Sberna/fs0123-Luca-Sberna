09731f52bab7a1b4d9b06be7572c908cad8f151c
Luca-Sberna <sberna.luca.ibiza@gmail.com>
2023-02-15 16:49:46 +0100

    compito

---
+++ m1/s3/g3/compito/js/script.js
@@ -0,0 +1,49 @@
+//variabili base
+const nuovaTaskInput = document.querySelector('#nuova-task');
+const aggiungiTaskBtn = document.querySelector('#aggiungi-task');
+const listeTask = document.querySelector('#task-list');
+
+//evento aggiungi task
+aggiungiTaskBtn.addEventListener('click', function () {
+
+    const nomeTask = nuovaTaskInput.value;
+    //se il campo testo è diverso da '' fai partire le funzioni
+    if (nomeTask.trim() !== '') {
+        const nuovaTask = document.createElement('li');
+        nuovaTask.innerText = nomeTask;
+
+
+        //delete button per le liste create dinamicamente
+        const deleteBtn = document.createElement("button");
+        deleteBtn.classList.add("delete-btn");
+        deleteBtn.innerText = "Elimina";
+        deleteBtn.addEventListener("click", function () {
+            listeTask.removeChild(divLi);
+        });
+
+
+        //aggiunta della classe css per sbarrare le liste al click su di esse
+        nuovaTask.addEventListener('click', function () {
+            nuovaTask.classList.toggle('completed')
+        });
+
+
+
+        //div che racchiude ogni lista creata
+        const divLi = document.createElement('div');
+        //aggiunge gli elementi nuova task e delete btn al div liste
+        divLi.appendChild(nuovaTask);
+        divLi.appendChild(deleteBtn);
+        //aggiunge ad un elemento div liste che contiene gli elementi citati sopra , alle liste task
+        listeTask.appendChild(divLi);
+        //azzera il valore dell input per far inserire altro all utente
+        nuovaTaskInput.value = "";
+
+
+
+
+    }
+
+
+});
+
