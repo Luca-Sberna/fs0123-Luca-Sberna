4e9e6b678c340b078e316f851dbc0b1a70d34605
Luca-Sberna <sberna.luca.ibiza@gmail.com>
2023-02-15 11:19:55 +0100

    teoria

---
+++ m1/s3/g3/script.js
@@ -0,0 +1,13 @@
+//calcolo distanza tra due punti 
+function distanceBetweenPoints(a, b) {
+    a = [9, 2];
+    b = [1, 8];
+    const distance = Math.sqrt((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2);
+
+
+    return document.write(distance);
+}
+
+//----------------------------------------------------------------
+
+
